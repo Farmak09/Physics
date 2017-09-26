5fa734f85fded76feaa70d1fc631004d0b4c583a
Farmak09 <farmak9@gmail.com>
2017-09-26 19:50:04 +0200

    done

--- ModulePhysics.cpp
+++ ModulePhysics.cpp
@@ -28,7 +28,7 @@ bool ModulePhysics::Start()
 {
 	LOG("Creating Physics 2D environment");
 	
-	warudo = new b2World(b2Vec2(0.0f, -9.81f));
+	warudo = new b2World(b2Vec2(0.0f, 9.81f));
 
 	// TODO 2: Create a private variable for the world
 	// - You need to send it a default gravity
@@ -36,9 +36,19 @@ bool ModulePhysics::Start()
 	// - Remember to destroy the world after using it
 
 	b2BodyDef groundBodyDef;
-	groundBodyDef.position.Set(SCREEN_WIDTH/2, SCREEN_HEIGHT/2);
+	groundBodyDef.position.Set(PIXEL_TO_METERS(SCREEN_WIDTH/2),PIXEL_TO_METERS(SCREEN_HEIGHT/2));
+	groundBodyDef.type = b2_staticBody;
 
-	// TODO 4: Create a a big static circle as "ground"
+	b2Body* groundBody = warudo->CreateBody(&groundBodyDef);
+
+	b2CircleShape groundShape;
+	groundShape.m_radius = PIXEL_TO_METERS(300);
+
+	b2FixtureDef groundFixture;
+	groundFixture.shape = &groundShape;
+
+	groundBody->CreateFixture(&groundFixture);
+	// TODO 4: Create a big static circle as "ground"
 	return true;
 }
 
@@ -57,6 +67,24 @@ update_status ModulePhysics::PostUpdate()
 	// TODO 5: On space bar press, create a circle on mouse position
 	// - You need to transform the position / radius
 
+	if (App->input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN)
+	{
+
+		b2BodyDef BodyDef;
+		BodyDef.position.Set(PIXEL_TO_METERS(SCREEN_WIDTH / 2 - 1), PIXEL_TO_METERS(0));
+		BodyDef.type = b2_dynamicBody;
+
+		b2Body* Body = warudo->CreateBody(&BodyDef);
+
+		b2CircleShape Shape;
+		Shape.m_radius = PIXEL_TO_METERS(50);
+
+		b2FixtureDef Fixture;
+		Fixture.shape = &Shape;
+
+		Body->CreateFixture(&Fixture);
+	}
+
 	if(App->input->GetKey(SDL_SCANCODE_F1) == KEY_DOWN)
 		debug = !debug;
 
@@ -65,8 +93,8 @@ update_status ModulePhysics::PostUpdate()
 
 	// Bonus code: this will iterate all objects in the world and draw the circles
 	// You need to provide your own macro to translate meters to pixels
-	/*
-	for(b2Body* b = world->GetBodyList(); b; b = b->GetNext())
+	
+	for(b2Body* b = warudo->GetBodyList(); b; b = b->GetNext())
 	{
 		for(b2Fixture* f = b->GetFixtureList(); f; f = f->GetNext())
 		{
@@ -76,14 +104,14 @@ update_status ModulePhysics::PostUpdate()
 				{
 					b2CircleShape* shape = (b2CircleShape*)f->GetShape();
 					b2Vec2 pos = f->GetBody()->GetPosition();
-					App->renderer->DrawCircle(METERS_TO_PIXELS(pos.x), METERS_TO_PIXELS(pos.y), METERS_TO_PIXELS(shape->m_radius), 255, 255, 255);
+					App->renderer->DrawCircle(METER_TO_PIXELS(pos.x), METER_TO_PIXELS(pos.y), METER_TO_PIXELS(shape->m_radius), 255, 255, 255);
 				}
 				break;
 
 				// You will have to add more cases to draw boxes, edges, and polygons ...
 			}
 		}
-	}*/
+	}
 
 	return UPDATE_CONTINUE;
 }
